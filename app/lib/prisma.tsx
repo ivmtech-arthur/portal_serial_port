d013838a5cd30516b49628c1aa1fc637952b36c5
arthur.lau@ivmtech.com <arthur.lau@ivmtech.com>
2023-10-10 16:31:17 +0800

    wip

--- app/lib/prisma.tsx
+++ app/lib/prisma.tsx
@@ -23,6 +23,167 @@ export const schemaMap = {
   // "": prisma
 }
 
+function createInclude(obj, includeValue: string) {
+  const tempObj = Object.assign({}, obj)
+  var reservedKey = [...new Set(includeValue.split(/[.,]+/))];
+  var result = iterate(tempObj, reservedKey[0], reservedKey)
+  return result
+}
+
+function iterate(instance, key2, reservedKey) {
+  var instance2 = instance;
+  if (key2 != "include") {
+    if (!reservedKey.includes(key2)) {
+      return null;
+    }
+  }
+  for (let key in instance) {
+    instance2[key] = iterate(instance[key], key, reservedKey);
+    if (!instance2[key]) {
+      delete instance2[key]
+    }
+    if (key == "include" && Object.keys(instance2[key]).length == 0) {
+      instance2 = true;
+    }
+  }
+  console.log("instance end", instance2, key2)
+  return instance2;
+}
+
+export function handleClause(collection, id, populate?,filters?) {
+  var allPopulate = false;
+  var singlePopulate = false;
+  var listPopulate = false;
+  var includeClause = {};
+  var whereClause: any;
+
+  if (populate && typeof populate === 'string') {
+    if (populate == "*") {
+      allPopulate = true;;
+
+    } else if (populate.split(',').length > 1) {
+      listPopulate = true;
+    } else {
+      singlePopulate = true;
+    }
+  }
+
+  if (filters) { 
+    console.log("filter",JSON.parse(filters));
+  }
+
+  switch (collection) {
+    case "machine":
+      var machineClause: Prisma.MachineWhereUniqueInput = {
+        machineID: id
+      }
+      whereClause = machineClause;
+
+      if (populate) {
+        var machineInclude: Prisma.MachineInclude = {
+
+        };
+
+        includeClause = {
+          ...(allPopulate ? machineInclude : {}),
+          ...(listPopulate ? {
+            ...(createInclude(machineInclude, populate))
+          } : {}),
+          ...(singlePopulate ? { ...(createInclude(machineInclude, populate)) } : {})
+        }
+      }
+      break
+    case "machinePalletDetail":
+      var machinePalletDetailClause: Prisma.MachinePalletDetailWhereUniqueInput = {
+        palletDetailID: id,
+      }
+      whereClause = machinePalletDetailClause;
+
+
+      break
+    case "machineProductSummary":
+      var machineProductSummaryClause: Prisma.MachineProductSummaryWhereUniqueInput = {
+        summaryID: id
+      }
+      whereClause = machineProductSummaryClause;
+
+      if (populate) {
+        var machineProductSummaryInclude: Prisma.MachineProductSummaryInclude = {
+
+        };
+
+        includeClause = {
+          ...(allPopulate ? machineProductSummaryInclude : {}),
+          ...(listPopulate ? {
+            ...(createInclude(machineProductSummaryInclude, populate))
+          } : {}),
+          ...(singlePopulate ? { ...(createInclude(machineProductSummaryInclude, populate)) } : {})
+        }
+      }
+      break
+    case "masterProduct":
+      var masterProductClause: Prisma.MasterProductWhereUniqueInput = {
+        productID: id
+      }
+      whereClause = masterProductClause;
+
+
+      if (populate) {
+        var productInclude: Prisma.MasterProductInclude = {
+
+        };
+
+        includeClause = {
+          ...(allPopulate ? productInclude : {}),
+          ...(listPopulate ? {
+            ...(createInclude(productInclude, populate))
+          } : {}),
+          ...(singlePopulate ? { ...(createInclude(productInclude, populate)) } : {})
+        }
+      }
+      break
+    case "user":
+      var userClause: Prisma.UserWhereUniqueInput = {
+        userID: id
+      }
+      whereClause = userClause
+
+      if (populate) {
+        var userInclude: Prisma.UserInclude = {
+          userSession: true,
+          machine: {
+            include: {
+              machinePalletDetail: {
+                include: {
+                  masterProduct: true
+                }
+              },
+              machineProductSummary: {
+                include: {
+                  masterProduct: true
+                }
+              }
+            }
+          },
+        };
+
+
+        includeClause = {
+          ...(allPopulate ? userInclude : {}),
+          ...(listPopulate ? {
+            ...(createInclude(userInclude, populate))
+          } : {}),
+          ...(singlePopulate ? { ...(createInclude(userInclude, populate)) } : {})
+        }
+      }
+      break
+    default:
+      break;
+
+  }
+  return { whereClause, includeClause }
+}
+
 export async function isAuthorised(token: string) {
  
   try{
--- app/pages/api/prisma/[collection]/index.tsx
+++ app/pages/api/prisma/[collection]/index.tsx
@@ -1,5 +1,35 @@
-import { isAuthorised, prisma } from "lib/prisma";
+import { isAuthorised, prisma, schemaMap,handleClause } from "lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+
+async function GET(req: NextApiRequest,res: NextApiResponse) { 
+    try {
+        console.log("req.query", req.query)
+        const { collection, id, populate,filters } = req.query
+        if (typeof collection === 'string' && typeof id === 'string') {
+            const { whereClause, includeClause } = handleClause(collection, id, populate,filters);
+            console.log("whereClause", whereClause, includeClause)
+            const result = await schemaMap["user"].findMany({
+
+                where: {
+                    userID: {
+                        // in
+                    }
+                },
+                include: includeClause
+            })
+            res.status(200).json({
+                result
+            })
+            return
+        }
+        res.end()
+    } catch (e) {
+        console.log("error", e);
+        res.status(400).json({
+            "error": e
+        })
+    }
+}
 
 export default async function handler(req: NextApiRequest,res: NextApiResponse){
     try{
@@ -8,10 +38,14 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse){
             const tokenAuthorized = await isAuthorised(authorization.replace("Bearer ",""));
             if(tokenAuthorized){
                 switch(req.method){
+                    case "GET":
+                        break;
+                    case "POST":
+                        break
+                    case "PUT":
+                        break;
                     case "DELETE":
                         break;
-                    case "PUT":
-                        break
                 }
             }
         }else{
